112ff88982499ce4fa93bb89bf442203bed7ac07
Denys <d.yefremov@polytech.software>
2021-02-11 18:47:09 +0200

    Initial Commit

---
+++ CatsFacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatsFacts
+//
+//  Created by DenysYefremov on 11.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
